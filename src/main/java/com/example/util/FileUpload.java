9b7dab737e3700de997b549e101845d544c263aa
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-11-18 02:03:28 +0900

    complete

---
+++ src/main/java/com/example/util/FileUpload.java
@@ -0,0 +1,56 @@
+package com.example.util;
+
+import com.example.dao.BoardDAO;
+import com.example.vo.BoardVO;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+public class FileUpload {
+    public BoardVO uploadPhoto(HttpServletRequest request) {
+        String filename = ""; //업로드 파일이름 저장
+        int sizeLimit = 15 * 1024 * 1024; //15MB
+
+        String realPath = request.getServletContext().getRealPath("upload");
+        File dir = new File(realPath);
+        if (!dir.exists()) dir.mkdirs();
+
+        BoardVO one = null;
+        MultipartRequest multipartRequest = null;
+        try {
+            multipartRequest = new MultipartRequest(request, realPath, sizeLimit, "utf-8", new DefaultFileRenamePolicy());
+            filename = multipartRequest.getFilesystemName("photo");
+            one = new BoardVO();
+            String seq = multipartRequest.getParameter("seq");
+            if(seq!=null&&!seq.equals("")) one.setSeq(Integer.parseInt(seq));
+            one.setCategory(multipartRequest.getParameter("category"));
+            one.setTitle(multipartRequest.getParameter("title"));
+            one.setWriter(multipartRequest.getParameter("writer"));
+            one.setContent(multipartRequest.getParameter("content"));
+
+            if(seq!=null&&!seq.equals("")) {    //수정인 경우
+                BoardDAO dao = new BoardDAO();
+                String oldfilename = dao.getPhotoFilename(Integer.parseInt(seq));
+                if(filename!=null && oldfilename!=null) {   //교체할 파일 존재
+                    FileUpload.deleteFile(request, oldfilename);
+                } else if (filename==null && oldfilename!=null) {   //교체할 파일이 없는 경우
+                    filename = oldfilename;
+                }
+            }
+            one.setPhoto(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return one;
+    }
+    public static void deleteFile(HttpServletRequest request, String filename) {
+        String filePath = request.getServletContext().getRealPath("upload");
+
+        File f = new File(filePath + "/" + filename);
+        if(f.exists()) f.delete();
+    }
+}
+
